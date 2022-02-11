22fde37494dc51a5fa0f4624b158e5b5796e569f
mplbruno <mplbruno@gmail.com>
2022-02-11 14:39:01 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DOTNET_POO.src.Entities;
+
+namespace DOTNET_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Knight arus = new Knight("Arus", 23, "Knight");
+           Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            
+
+         
+           Console.WriteLine(wizard.Attack(1)); 
+           Console.WriteLine(wizard.Attack(7)); 
+          
+        }
+    }
+}
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,26 @@
+namespace DOTNET_POO.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        public override string Attack()
+        {
+            return this.Name + " Lançou Magia";
+        }
+
+         public string Attack(int Bonus)
+        {   if (Bonus > 6){ 
+            return this.Name + " Lançou Magia super efetiva com bonus de ataque de " + Bonus;
+        }else
+        {
+            return this.Name + " Lançou Magia com força fraca com bonus de ataque de " + Bonus;
+        }
+    }
+
+}
+}
